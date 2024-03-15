9c27976095f86cdc0e0cb6f86ff1f911414c9373
András Tóth <andras.toth@acasus.com>
2024-03-14 21:19:06 -0600

    finalize TimeControls component (without the Poppins font though)

--- components/time-controls.tsx
+++ components/time-controls.tsx
@@ -1,4 +1,8 @@
-import {Button, View} from 'react-native';
+import { Button, StyleSheet, View } from 'react-native';
+
+const JANUARY = 0;
+const DECEMBER = 11;
+const NUMBER_OF_MONTHS_IN_A_YEAR = 12;
 
 interface TimeControlsProps {
   year: number;
@@ -9,15 +13,37 @@ interface TimeControlsProps {
 
 export function TimeControls({year, month, setYear, setMonth}: TimeControlsProps) {
   function decrementMonth() {
+    if (month === JANUARY) {
+      setMonth(DECEMBER);
+      setYear(year - 1);
+      return;
+    }
 
+    setMonth(month - 1);
   }
 
   function incrementMonth() {
+    setMonth((month + 1) % NUMBER_OF_MONTHS_IN_A_YEAR);
 
+    if (month === DECEMBER) {
+      setYear(year + 1);
+    }
   }
 
-  return <View>
-    <Button title="&gt;" onPress={decrementMonth}/>
-    <Button title="&lt;" onPress={incrementMonth}/>
+  return <View style={styles.container}>
+    <Button color="#00000000" title="&lt;" onPress={decrementMonth}/>
+    <Button color="#00000000" title="&gt;" onPress={incrementMonth}/>
   </View>
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  buttonStyle: {
+    backgroundColor: '#00000000'
+  }
+});
+
