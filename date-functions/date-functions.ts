2d04cfb8786b7e3db4ddad99b148ff7aa02057d1
András Tóth <andras.toth@acasus.com>
2024-03-14 22:22:48 -0600

    create date functions (probably buggy, will be fixed later)

---
+++ date-functions/date-functions.ts
@@ -0,0 +1,54 @@
+export enum JumpOperation {
+  Stay,
+  Backward,
+  Forward,
+}
+
+export type Day = {
+  operation: JumpOperation,
+  day: number,
+}
+
+const TOTAL_NUMBER_OF_DAYS_PER_PAGE = 5 * 7;
+
+// defining it as an object with named properties it is harder to pass year for month and vice versa
+interface GetDaysForPageParams {
+  year: number;
+  month: number;
+}
+
+export function getDaysForPage({ year, month }: GetDaysForPageParams): Day[] {
+  const offset = getOffsetOfFirstDay(year, month);
+
+  const arrayOfCurrentMonth = numberOfDaysToDayArray(getDaysInMonth(year, month)).map(day => ({
+    operation: JumpOperation.Stay,
+    day,
+  }));
+
+  const arrayOfPreviousMonth = numberOfDaysToDayArray(getDaysInMonth(year, month - 1)).slice(-offset).map(day => ({
+    operation: JumpOperation.Backward,
+    day,
+  }));
+
+  const arrayOfNextMonth = numberOfDaysToDayArray(getDaysInMonth(year, month + 1)).map(day => ({
+    operation: JumpOperation.Forward,
+    day,
+  }));
+
+  return [...arrayOfPreviousMonth, ...arrayOfCurrentMonth, ...arrayOfNextMonth].slice(0, TOTAL_NUMBER_OF_DAYS_PER_PAGE);
+}
+
+function getOffsetOfFirstDay(year: number, month: number) {
+  const firstDay = new Date(year, month, 1);
+
+  return firstDay.getDay();
+}
+
+function numberOfDaysToDayArray(numberOfDays: number) {
+  return Array.from({length: numberOfDays}, (_, i) => i + 1)
+}
+
+// modified stack overflow answer
+function getDaysInMonth(year: number, month: number) {
+  return new Date(year, month + 1, 0).getDate();
+}
