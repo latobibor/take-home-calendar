e0bee44f6cf3ae9d39fb4eca9ee22ec7821cb9ca
András Tóth <andras.toth@acasus.com>
2024-03-25 23:10:10 -0600

    move importing fonts into its own file so abstraction levels are missed

---
+++ components/hooks/use-design-fonts.tsx
@@ -0,0 +1,21 @@
+import { useFonts } from 'expo-font';
+
+type UseDesignFontsReturnValue = {
+  areFontsBeingLoaded: boolean;
+}
+
+export function useDesignFonts(): UseDesignFontsReturnValue {
+  const [fontsLoaded, fontError] = useFonts({
+    // I decided against moving out `../../assets/fonts` into a variable
+    // as it would just obscure the file paths without adding much benefit
+    'Poppins-Medium': require('../../assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-SemiBold': require('../../assets/fonts/Poppins-SemiBold.ttf'),
+    'Poppins-ExtraLight': require('../../assets/fonts/Poppins-ExtraLight.ttf'),
+  });
+
+  if (!fontsLoaded && fontError) {
+    console.error('Fonts could not have been loaded because of an error', fontError);
+  }
+
+  return { areFontsBeingLoaded: !fontsLoaded };
+}
