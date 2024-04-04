464c3b1c8a333b05148987ccc63588cb6c6b58bc
András Tóth <andras.toth@acasus.com>
2024-04-04 21:01:16 +0200

    fix broken path to fonts

--- hooks/use-design-fonts.tsx
+++ hooks/use-design-fonts.tsx
@@ -6,11 +6,11 @@ type UseDesignFontsReturnValue = {
 
 export function useDesignFonts(): UseDesignFontsReturnValue {
   const [fontsLoaded, fontError] = useFonts({
-    // I decided against moving out `../../assets/fonts` into a variable
+    // I decided against moving out `../assets/fonts` into a variable
     // as it would just obscure the file paths without adding much benefit
-    'Poppins-Medium': require('../../assets/fonts/Poppins-Medium.ttf'),
-    'Poppins-SemiBold': require('../../assets/fonts/Poppins-SemiBold.ttf'),
-    'Poppins-ExtraLight': require('../../assets/fonts/Poppins-ExtraLight.ttf'),
+    'Poppins-Medium': require('../assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-SemiBold': require('../assets/fonts/Poppins-SemiBold.ttf'),
+    'Poppins-ExtraLight': require('../assets/fonts/Poppins-ExtraLight.ttf'),
   });
 
   if (!fontsLoaded && fontError) {
